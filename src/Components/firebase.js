e690d514619628f7f98b82368d5f66782f6f508f
Aaron-Wright <georonathan47@gmail.com>
2021-03-07 22:47:18 +0000

    firebase

---
+++ src/Components/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhMIJ9K22MLDHQnknWQ1h89ScLWREkUlI",
+  authDomain: "portfolio-ebc39.firebaseapp.com",
+  projectId: "portfolio-ebc39",
+  storageBucket: "portfolio-ebc39.appspot.com",
+  messagingSenderId: "935079231440",
+  appId: "1:935079231440:web:51c4aefcfa25b7f34843d5"
+};
