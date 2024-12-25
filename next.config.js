50ff80e3720b964af34d32e4d014f2e426b3b76a
Jr.Osafo <georonathan47@gmail.com>
2024-12-25 04:38:11 +0000

    👷 setup pipelines for auto deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  basePath: "/portfolio",
   images: {
     unoptimized: true
   }
